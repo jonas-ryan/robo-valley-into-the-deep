d85d65a656187c771d8207fd9a6c2e092828f92c
jonas-ryan <j.e.ryan2000@gmail.com>
2024-11-03 21:58:05 -0800

    Add worm gear and linear slide resetting controls, a slide and worm gear limit override button, and a 40 inch horizontal extension limit.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robovalley/MecanumDriveRobotCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robovalley/MecanumDriveRobotCentric.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -12,12 +13,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class MecanumDriveRobotCentric extends LinearOpMode {
 
     private double slowSpeed = 0.333;
-    private double speedMultiplier = 1.0;
+    private double speedMultiplier = 0.666;
     private double slideSlowSpeed = 0.333;
     private double wormSlowSpeed = 0.333;
     private int slideSafetyMaximum = 4200;
     private int slideSafetyMinimum = 500;
     private double slideSafetySpeed = 0.2;
+    private double wormDegreesPerTick = 0.0250347705146036;
+    private double slideInchesPerTick = 0.0086977530804542;
 
     private DcMotor leftFront = null;
     private DcMotor leftBack = null;
@@ -48,6 +51,8 @@ public class MecanumDriveRobotCentric extends LinearOpMode {
         wormGear = hardwareMap.get(DcMotor.class, "wormGear");
         wormGear.setDirection(DcMotorSimple.Direction.FORWARD);
         wormGear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        wormGear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wormGear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         linearSlide = hardwareMap.get(DcMotor.class, "linearSlide");
         linearSlide.setDirection(DcMotorSimple.Direction.REVERSE);
         linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -65,6 +70,9 @@ public class MecanumDriveRobotCentric extends LinearOpMode {
 
         while (opModeIsActive()) {
 
+
+            /* CONTROLS */
+
             double controlAxial = -gamepad1.right_stick_y;
             double controlLateral =  gamepad1.right_stick_x;
             double controlYaw =  gamepad1.left_stick_x;
@@ -77,8 +85,13 @@ public class MecanumDriveRobotCentric extends LinearOpMode {
             double controlClawRotation =  gamepad2.right_trigger - gamepad2.left_trigger;
             boolean controlClawOpen = gamepad2.b;
             boolean controlClawClosed = gamepad2.a;
+            boolean controlSlideOverride = gamepad2.right_bumper;
 
-            // DRIVETRAIN BLOCK
+            boolean controlResetSlide = (gamepad1.dpad_down && gamepad1.dpad_right) || (gamepad2.dpad_down && gamepad2.dpad_right);
+            boolean controlResetWormGear = (gamepad1.dpad_down && gamepad1.dpad_left) || (gamepad2.dpad_down && gamepad2.dpad_left);
+
+
+            /* DRIVETRAIN CODE */
 
             if (controlSlow) {
                 controlAxial *= slowSpeed;
@@ -121,27 +134,29 @@ public class MecanumDriveRobotCentric extends LinearOpMode {
 
 
             /*   ARM CODE   */
-            // if (slide > 0.8) {
-            //     slide = slide * 5 - 3.8;
-            // } else if (slide < 0.8) {
-            //     slide = slide * 5 + 3.8;
-            // } else {
-            //     slide = 0.4 * slide;
-            // }
 
             // Reduces slide and worm gear speeds if the slow button is held.
             if (controlSlideSlow) {
                 controlSlide *= slideSlowSpeed;
                 controlWorm *= wormSlowSpeed;
             }
-            // Checks if the linear slide is past the maximum and then if power is trying to be applied in that direction, set it to zero.
-            if (linearSlide.getCurrentPosition() > slideSafetyMaximum && controlSlide > 0) {
+            // Checks if the linear slide is past the maximum extension and then if power is trying to be applied in that direction, set it to zero. There is also an override button that it checks for.
+            if (linearSlide.getCurrentPosition() > slideSafetyMaximum && controlSlide > 0 && !controlSlideOverride) {
                 controlSlide = 0;
             }
-            // Checks if the linear slide is below a certain threshold and then limits the speed of the motor if it is being powered towards the starting position.
-            if (linearSlide.getCurrentPosition() < slideSafetyMinimum && controlSlide < 0) {
+            // Checks if the linear slide is below a certain threshold and then limits the speed of the motor if it is being powered towards the starting position. There is also an override button that it checks for.
+            if (linearSlide.getCurrentPosition() < slideSafetyMinimum && controlSlide < 0 && !controlSlideOverride) {
                 controlSlide *= slideSafetySpeed;
             }
+
+            // Checks if the current horizontal extension is greater than 20 (using some trigonometry) and then only let the slide retract.
+            if ((linearSlide.getCurrentPosition() * slideInchesPerTick * Math.cos(Math.toRadians(wormGear.getCurrentPosition() * wormDegreesPerTick))) > 19.5 && !controlSlideOverride) {
+                if (controlSlide > 0) {
+                    controlSlide = 0;
+                }
+                controlWorm = 0;
+            }
+
             // Sets the powers of the slide and worm gear.
             linearSlide.setPower(controlSlide);
             wormGear.setPower(controlWorm);
@@ -149,13 +164,32 @@ public class MecanumDriveRobotCentric extends LinearOpMode {
             // Rotates the wrist based on the triggers.
             clawWrist.setPower(controlClawRotation * 0.1);
 
+            // Checks if the claw buttons are pressed and sets the position of the claw accordingly.
             if (controlClawOpen) {
                 claw.setPosition(0.25);
             } else if (controlClawClosed) {
                 claw.setPosition(0);
             }
 
+
+            /* CONFIG BUTTONS */
+
+            // Resets the encoder positions of the slide or worm gear based on their according buttons.
+            if(controlResetWormGear) {
+                wormGear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                wormGear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
+            if(controlResetSlide) {
+                linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                linearSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
+
             // Telemetry updates in case there is any telemetry that needs to be displayed.
+            telemetry.addData("Worm Gear Ticks", wormGear.getCurrentPosition());
+            telemetry.addData("Linear Slide Ticks", linearSlide.getCurrentPosition());
+            telemetry.addData("Worm Gear Degrees", wormGear.getCurrentPosition() * wormDegreesPerTick);
+            telemetry.addData("Linear Slide Inches", linearSlide.getCurrentPosition() * slideInchesPerTick);
+            telemetry.addData("Horizontal Slide Extension", (linearSlide.getCurrentPosition() * slideInchesPerTick * Math.cos(Math.toRadians(wormGear.getCurrentPosition() * wormDegreesPerTick))));
             telemetry.update();
         }
     }}
