35a0d4f335ea79a3b44b910520e4eb2f7f195ecc
jonas-ryan <j.e.ryan2000@gmail.com>
2024-12-24 21:15:21 -0800

    Add variable auto test. This OpMode tries to make a variable that is then used by other autos.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robovalley/AutoSetter.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.robovalley;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="AutoSetter")
+public class AutoSetter extends LinearOpMode {
+
+    private Persist persist = new Persist();
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                persist.setTime(persist.getTime() + 0.5);
+            }
+
+            if (gamepad1.b) {
+                persist.setTime(persist.getTime() - 0.5);
+            }
+
+            telemetry.addData("Time", persist.getTime());
+            telemetry.update();
+        }
+    }
+
+    public static class Persist {
+        private static double time = 0;
+
+        public double getTime() {
+            return time;
+        }
+        public void setTime(Double timeSet) {
+            time = timeSet;
+        }
+    }
+}
