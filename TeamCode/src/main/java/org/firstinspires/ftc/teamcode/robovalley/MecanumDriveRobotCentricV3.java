1c6cd20a54177a602a3c0599798cbd7eb2a183e7
jonas-ryan <j.e.ryan2000@gmail.com>
2025-02-04 22:01:09 -0800

    Add HashMap so that very redundant linear slide state machine code could be simplified. Add code so that the virtual four bar now moves. Rename a few variables and do some reorganization.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robovalley/MecanumDriveRobotCentricV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robovalley/MecanumDriveRobotCentricV3.java
@@ -8,7 +8,9 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="Robot Centric", group="Mecanum Drive")
+import java.util.HashMap;
+
+@TeleOp(name="Robot Centric v2", group="Mecanum Drive")
 //@Disabled
 public class MecanumDriveRobotCentricV3 extends LinearOpMode {
 
@@ -35,6 +37,7 @@ public class MecanumDriveRobotCentricV3 extends LinearOpMode {
     private final int SLIDES_HEIGHT_WALL = 1100;
     private final int SLIDES_HEIGHT_FLOOR = 350;
     private final int SLIDES_HEIGHT_CHAMBER = 2850;
+    private HashMap<slideHeight, Integer> slidesHeights = new HashMap<>();
 
     /* DRIVETRAIN MOTORS */
     private DcMotorEx leftFront = null;
@@ -45,9 +48,9 @@ public class MecanumDriveRobotCentricV3 extends LinearOpMode {
     /* SLIDES MOTORS */
     private DcMotorEx backSlide = null;
     private DcMotorEx frontSlide = null;
-    private DcMotorEx perpSlide = null;
+    private DcMotorEx perpendicularSlide = null;
 
-    /* INTAKE MOTORS */
+    /* INTAKE SERVOS */
     private Servo wrist = null;
     private CRServo leftIntake = null;
     private CRServo rightIntake = null;
@@ -55,7 +58,7 @@ public class MecanumDriveRobotCentricV3 extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-
+        /* DRIVETRAIN CONFIGURATION */
         leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
         leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
         rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
@@ -68,31 +71,34 @@ public class MecanumDriveRobotCentricV3 extends LinearOpMode {
         leftBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        wrist = hardwareMap.get(Servo.class, "wrist");
 
+        /* SLIDES CONFIGURATION */
         backSlide = hardwareMap.get(DcMotorEx.class, "backSlide");
         frontSlide = hardwareMap.get(DcMotorEx.class, "frontSlide");
-        perpSlide = hardwareMap.get(DcMotorEx.class, "perpSlide");
+        perpendicularSlide = hardwareMap.get(DcMotorEx.class, "perpSlide");
         backSlide.setDirection(DcMotorSimple.Direction.REVERSE);
         frontSlide.setDirection(DcMotorSimple.Direction.REVERSE);
-        perpSlide.setDirection(DcMotorSimple.Direction.REVERSE);
-        //backSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //frontSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //perpSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        perpendicularSlide.setDirection(DcMotorSimple.Direction.REVERSE);
         backSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        perpSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-
-
+        perpendicularSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        perpSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        perpendicularSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        /* INTAKE CONFIGURATION */
         leftIntake = hardwareMap.get(CRServo.class, "intakeA");
         rightIntake = hardwareMap.get(CRServo.class, "intakeB");
         leftIntake.setDirection(CRServo.Direction.REVERSE);
         rightIntake.setDirection(CRServo.Direction.FORWARD);
+        wrist = hardwareMap.get(Servo.class, "wrist");
 
+        /* HASHMAP CONFIGURATION */
+        slidesHeights.put(slideHeight.WALL, SLIDES_HEIGHT_WALL);
+        slidesHeights.put(slideHeight.FLOOR, SLIDES_HEIGHT_FLOOR);
+        slidesHeights.put(slideHeight.CHAMBER, SLIDES_HEIGHT_CHAMBER);
+
+        /* TELEMETRY */
         telemetry.addData("Status", "Initialized");
         telemetry.addData("Mode", "Robot Centric");
         telemetry.update();
@@ -100,33 +106,34 @@ public class MecanumDriveRobotCentricV3 extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-
-
-            /* CONTROLS */
-
+            /* DRIVETRAIN CONTROLS */
             double controlAxial = -gamepad1.right_stick_y;
             double controlLateral = gamepad1.right_stick_x;
             double controlYaw = gamepad1.left_stick_x;
             boolean controlSlow = gamepad1.left_bumper;
             boolean controlReverse = gamepad1.right_bumper;
-            double controlWristLower = gamepad1.right_trigger;
-            double controlWristRaise = gamepad1.left_trigger;
 
-            double controlVertSlide = -gamepad2.left_stick_y;
-            double controlPerpSlide = -gamepad2.right_stick_y;
-            double controlIntake =  gamepad2.right_trigger - gamepad2.left_trigger;
+            /* SLIDES CONTROLS */
+            double controlVerticalSlide = -gamepad2.left_stick_y;
+            double controlPerpendicularSlide = -gamepad2.right_stick_y;
             boolean controlSlideOverride = gamepad2.dpad_up;
-            boolean controlResetVertSlide = gamepad1.dpad_down && gamepad1.dpad_right || gamepad2.dpad_down && gamepad2.dpad_right;
-            boolean controlResetPerpSlide = gamepad1.dpad_down && gamepad1.dpad_left || gamepad2.dpad_down && gamepad2.dpad_left;
+            boolean controlResetVerticalSlide = gamepad1.dpad_down && gamepad1.dpad_right || gamepad2.dpad_down && gamepad2.dpad_right;
+            boolean controlResetPerpendicularSlide = gamepad1.dpad_down && gamepad1.dpad_left || gamepad2.dpad_down && gamepad2.dpad_left;
             boolean controlSlideFloor = gamepad2.a;
             boolean controlSlideChamber = gamepad2.y;
             boolean controlSlideWall = gamepad2.b;
             boolean controlSlideDone = gamepad2.x;
             boolean controlSlideSlow = gamepad2.right_bumper;
 
+            /* INTAKE CONTROLS */
+            double controlIntake =  gamepad2.right_trigger - gamepad2.left_trigger;
+            boolean controlWristRaise = gamepad1.y;
+            boolean controlWristFlat = gamepad1.x;
+            boolean controlWristLower = gamepad1.a;
+
+            ////////////////////
 
             /* DRIVETRAIN CODE */
-
             if (controlSlow) {
                 controlAxial *= DRIVETRAIN_SLOW_SPEED;
                 controlLateral *= DRIVETRAIN_SLOW_SPEED;
@@ -136,19 +143,16 @@ public class MecanumDriveRobotCentricV3 extends LinearOpMode {
                 controlAxial *= -1;
                 controlLateral *= -1;
             }
-
-            double max;
+            controlAxial *= DRIVETRAIN_SPEED_MULTIPLIER;
+            controlLateral *= DRIVETRAIN_SPEED_MULTIPLIER;
+            controlYaw *= DRIVETRAIN_SPEED_MULTIPLIER;
 
             double leftFrontPower = controlAxial + controlLateral + controlYaw;
             double leftBackPower = controlAxial - controlLateral + controlYaw;
             double rightBackPower = controlAxial + controlLateral - controlYaw;
             double rightFrontPower = controlAxial - controlLateral - controlYaw;
 
-            leftFrontPower *= DRIVETRAIN_SPEED_MULTIPLIER;
-            leftBackPower *= DRIVETRAIN_SPEED_MULTIPLIER;
-            rightBackPower *= DRIVETRAIN_SPEED_MULTIPLIER;
-            rightFrontPower *= DRIVETRAIN_SPEED_MULTIPLIER;
-
+            double max;
             max = Math.max(Math.abs(leftFrontPower), Math.abs(leftBackPower));
             max = Math.max(max, Math.abs(rightBackPower));
             max = Math.max(max, Math.abs(rightFrontPower));
@@ -166,20 +170,7 @@ public class MecanumDriveRobotCentricV3 extends LinearOpMode {
             rightBack.setPower(rightBackPower);
             rightFront.setPower(rightFrontPower);
 
-
-            /*   ARM CODE   */
-
-            if (controlWristRaise > 0.5) {
-                wrist.setPosition(0.3);
-            }
-            if (controlWristLower > 0.5) {
-                wrist.setPosition(0.01);
-            }
-
-
-            leftIntake.setPower(controlIntake);
-            rightIntake.setPower(controlIntake);
-
+            /* SLIDES CODE */
             if (controlSlideFloor) {
                 currentSlideHeight = slideHeight.FLOOR;
             }
@@ -193,51 +184,29 @@ public class MecanumDriveRobotCentricV3 extends LinearOpMode {
                 currentSlideHeight = slideHeight.WALL;
             }
             if (currentSlideHeight == slideHeight.DONE) {
-                if (backSlide.getCurrentPosition() > VERTICAL_SLIDE_SAFETY_MAXIMUM && controlVertSlide > 0 && !controlSlideOverride) {
+                if (backSlide.getCurrentPosition() > VERTICAL_SLIDE_SAFETY_MAXIMUM && controlVerticalSlide > 0 && !controlSlideOverride) {
                     backSlide.setPower(0);
                     frontSlide.setPower(0);
-                } else if (backSlide.getCurrentPosition() < 270 && controlVertSlide < 0 && !controlSlideOverride) {
+                } else if (backSlide.getCurrentPosition() < 0 && controlVerticalSlide < 0 && !controlSlideOverride) {
                     backSlide.setPower(0);
                     frontSlide.setPower(0);
-                } else if (backSlide.getCurrentPosition() < VERTICAL_SLIDE_SAFETY_MINIMUM && controlVertSlide < 0 && !controlSlideOverride) {
-                    backSlide.setPower(controlVertSlide * VERTICAL_SLIDE_SAFETY_SPEED);
-                    frontSlide.setPower(controlVertSlide * VERTICAL_SLIDE_SAFETY_SPEED);
+                } else if (backSlide.getCurrentPosition() < VERTICAL_SLIDE_SAFETY_MINIMUM && controlVerticalSlide < 0 && !controlSlideOverride) {
+                    backSlide.setPower(controlVerticalSlide * VERTICAL_SLIDE_SAFETY_SPEED);
+                    frontSlide.setPower(controlVerticalSlide * VERTICAL_SLIDE_SAFETY_SPEED);
                 } else {
-                    backSlide.setPower(controlVertSlide);
-                    frontSlide.setPower(controlVertSlide);
+                    backSlide.setPower(controlVerticalSlide);
+                    frontSlide.setPower(controlVerticalSlide);
                 }
             } else {
-                if (currentSlideHeight == slideHeight.WALL) {
-                    if (Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_WALL) < 50) {
+                if (slidesHeights.get(currentSlideHeight) != null) {
+                    if (Math.abs(backSlide.getCurrentPosition() - slidesHeights.get(currentSlideHeight)) < 50) {
                         currentSlideHeight = slideHeight.DONE;
-                    } else if (backSlide.getCurrentPosition() < SLIDES_HEIGHT_WALL) {
-                        backSlide.setPower((Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_WALL) / 400.0));
-                        frontSlide.setPower((Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_WALL) / 400.0));
-                    } else if (backSlide.getCurrentPosition() > SLIDES_HEIGHT_WALL) {
-                        backSlide.setPower(-(Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_WALL) / 400.0));
-                        frontSlide.setPower(-(Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_WALL) / 400.0));
-                    }
-                }
-                if (currentSlideHeight == slideHeight.CHAMBER) {
-                    if (Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_CHAMBER) < 50) {
-                        currentSlideHeight = slideHeight.DONE;
-                    } else if (backSlide.getCurrentPosition() < SLIDES_HEIGHT_CHAMBER) {
-                        backSlide.setPower((Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_CHAMBER) / 400.0));
-                        frontSlide.setPower((Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_CHAMBER) / 400.0));
-                    } else if (backSlide.getCurrentPosition() > SLIDES_HEIGHT_CHAMBER) {
-                        backSlide.setPower(-(Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_CHAMBER) / 400.0));
-                        frontSlide.setPower(-(Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_CHAMBER) / 400.0));
-                    }
-                }
-                if (currentSlideHeight == slideHeight.FLOOR) {
-                    if (Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_FLOOR) < 50) {
-                        currentSlideHeight = slideHeight.DONE;
-                    } else if (backSlide.getCurrentPosition() < SLIDES_HEIGHT_FLOOR) {
-                        backSlide.setPower((Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_FLOOR) / 400.0));
-                        frontSlide.setPower((Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_FLOOR) / 400.0));
-                    } else if (backSlide.getCurrentPosition() > SLIDES_HEIGHT_FLOOR) {
-                        backSlide.setPower(-(Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_FLOOR) / 400.0));
-                        frontSlide.setPower(-(Math.abs(backSlide.getCurrentPosition() - SLIDES_HEIGHT_FLOOR) / 400.0));
+                    } else if (backSlide.getCurrentPosition() < slidesHeights.get(currentSlideHeight)) {
+                        backSlide.setPower((Math.abs(backSlide.getCurrentPosition() - slidesHeights.get(currentSlideHeight)) / 400.0));
+                        frontSlide.setPower((Math.abs(backSlide.getCurrentPosition() - slidesHeights.get(currentSlideHeight)) / 400.0));
+                    } else if (backSlide.getCurrentPosition() > slidesHeights.get(currentSlideHeight)) {
+                        backSlide.setPower(-(Math.abs(backSlide.getCurrentPosition() - slidesHeights.get(currentSlideHeight)) / 400.0));
+                        frontSlide.setPower(-(Math.abs(backSlide.getCurrentPosition() - slidesHeights.get(currentSlideHeight)) / 400.0));
                     }
                 }
             }
@@ -252,35 +221,47 @@ public class MecanumDriveRobotCentricV3 extends LinearOpMode {
                 frontSlide.setPower(frontSlide.getPower() + VERTICAL_SLIDE_BRAKE_POWER);
             }
 
-            if (perpSlide.getCurrentPosition() > PERPENDICULAR_SLIDE_MAXIMUM && controlPerpSlide > 0 && !controlSlideOverride) {
-                perpSlide.setPower(0);
-            } else if (perpSlide.getCurrentPosition() < PERPENDICULAR_SLIDE_MINIMUM && controlPerpSlide < 0 && !controlSlideOverride) {
-                perpSlide.setPower(controlPerpSlide * PERPENDICULAR_SLIDE_MINIMUM_SPEED);
+            if (perpendicularSlide.getCurrentPosition() > PERPENDICULAR_SLIDE_MAXIMUM && controlPerpendicularSlide > 0 && !controlSlideOverride) {
+                perpendicularSlide.setPower(0);
+            } else if (perpendicularSlide.getCurrentPosition() < PERPENDICULAR_SLIDE_MINIMUM && controlPerpendicularSlide < 0 && !controlSlideOverride) {
+                perpendicularSlide.setPower(controlPerpendicularSlide * PERPENDICULAR_SLIDE_MINIMUM_SPEED);
             } else {
-                perpSlide.setPower(controlPerpSlide);
+                perpendicularSlide.setPower(controlPerpendicularSlide);
             }
 
+            /* INTAKE CODE */
+            if (controlWristRaise) {
+                wrist.setPosition(0.3);
+            } else if (controlWristFlat) {
+                wrist.setPosition(0.6);
+            } else if (controlWristLower) {
+                wrist.setPosition(0.95);
+            }
+
+            wrist.setPosition(wrist.getPosition() + (gamepad1.right_trigger - gamepad1.left_trigger) * 0.015);
+
+            leftIntake.setPower(controlIntake);
+            rightIntake.setPower(controlIntake);
 
             /* CONFIG BUTTONS */
-
-            if (controlResetVertSlide) {
+            if (controlResetVerticalSlide) {
                 backSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 frontSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 backSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                 frontSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             }
 
-            if (controlResetPerpSlide) {
-                perpSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                perpSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            if (controlResetPerpendicularSlide) {
+                perpendicularSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                perpendicularSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             }
 
             // Telemetry updates in case there is any telemetry that needs to be displayed.
-            telemetry.addLine(Float.toString(-gamepad2.left_stick_y));
-            telemetry.addLine(Float.toString(gamepad2.left_stick_x));
-            telemetry.addLine(Float.toString(backSlide.getCurrentPosition()));
-            telemetry.addLine(Float.toString(frontSlide.getCurrentPosition()));
-            telemetry.addLine(Float.toString(perpSlide.getCurrentPosition()));
+            telemetry.addData("Back Slide", Float.toString(backSlide.getCurrentPosition()));
+            telemetry.addData("Front Slide", Float.toString(frontSlide.getCurrentPosition()));
+            telemetry.addData("Perpendicular Slide", Float.toString(perpendicularSlide.getCurrentPosition()));
+            telemetry.addData("Runtime", getRuntime());
+            telemetry.addData("Servo Position", wrist.getPosition());
             telemetry.update();
         }
     }}
